e03bd29bf41075552de7b18fb7f40ca215f55be2
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-05-14 13:57:14 -0500

    Files creation

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+// Consumir API
+const API = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=00";
+
+const getData = (api) => {
+    return fetch(api)
+    .then((response) => response.json())
+    .then((json) => {
+        fetchImage(json),
+        paginacion(json.prev, json.next)
+    })
+    .catch((error) => {
+        console.log("Fetch Data Error: ", error)
+    })
+};
+
+const fetchImage = (data) => {
+    document.getElementById("datosPersonaje").innerHTML = "";
+    data.results.forEach((pokemon) => {
+        const url = pokemon.url;
+        return fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            displayName(json)
+        })
+        .catch((error) => {
+            console.log("Fetch Image Error: ", error)
+        })
+    })
+
+}
+
+// Dibujar cards de personajes
+const displayName = (data) => {
+    html = `
+    <div class="col-md-3">
+        <div class="card poke-image" id="poke-image" style="width: 18rem;">
+        <img src="${data.sprites.other["official-artwork"].front_default}" class="card-img-top" alt=""></img>
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text"><strong>Peso </strong>${data.weight}</p>
+            </div>
+        </div>
+    </div>
+    `
+    document.getElementById("datosPersonaje").innerHTML += html;
+};
+
+// Paginación
+const paginacion = (prev, next) => {
+    let html = "";
+    html += `<li class="page-item ${prev ? "" : 'disabled'}"><a class="page-link" onclick="(getData('${prev}'))">Prev</a></li>`;
+    html += `<li><a class="page-link ${next ? "" : 'disabled'}" onclick="(getData('${next}'))">Next</a></li>`;
+    document.getElementById("paginacion").innerHTML = html;
+};
+
+// Ejecutar getData
+getData(API);
